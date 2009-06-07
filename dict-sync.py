3871fed671a933bfe1216dc16d6e08cdb8140ba7
unknown <yd@.(none)>
2009-06-07 11:15:49 +0800

    update

--- dict-sync.py
+++ dict-sync.py
@@ -14,27 +14,20 @@ from google.appengine.api import urlfetch, memcache
 from google.appengine.ext.webapp import template
 
 class TaskMessage(db.Model):
-  queue_name = db.StringProperty()
-  type = db.IntegerProperty(required=True)
-  url = db.StringProperty()
-  offset = db.IntegerProperty()
-  size = db.IntegerProperty()
-  totallen = db.IntegerProperty()
+  queue_name = db.StringProperty(required=True)
+  url = db.StringProperty(required=True)
+  offset = db.IntegerProperty(required=True)
+  totallen = db.IntegerProperty(required=True)
   timestamp = db.DateTimeProperty(required=True, auto_now=True)
 
 class MyQueue():
-  BEGIN_FETCH=0
-  FETCHING=1
-  BEGIN_PROCESS=2
-  PROCESSING=3
-
   def push(queue_name, **kwargs):
     TaskMessage(queue_name=queue_name, **kwargs).put()
   push=staticmethod(push)
 
   def pop(queue_name):
     results = TaskMessage.gql("ORDER BY timestamp ASC LIMIT 1") or [None]
-    return result[0]
+    return results[0]
   pop=staticmethod(pop)
 
 
@@ -54,7 +47,28 @@ def get_my_config():
 class DictionarySync(webapp.RequestHandler):
   def get(self):
     config = get_my_config()
-    self.response.out.write('This is only a test')
+    for dic in config['dictlist']:
+      name = dic['name']
+      task = MyQueue.pop(name)
+      if task is not None: break;
+    if task is None:
+      self.response.out.write('No task in queue')
+      return
+    self.response.out.write('<p>queue=%s, url=%s, offset=%d, totallen=%d, timestamp=%s</p>' % (task.queue_name, task.url, task.offset, task.totallen, task.timestamp))
+    try:
+      result = urlfetch.fetch(url=url,
+                              method=urlfetch.GET,
+                              headers={'Range': 'bytes=%d-' % (task.offset)})
+      assert result.status_code == 206
+      assert result.headers.has_key('Content-Range')
+    except DownloadError, e:
+      self.response.out.write(`e` + '<br>')
+      pass
+    except Exception, e:
+      logging.error('Failed updating %s: %s' % (dic['name'], `e`))
+      self.response.out.write('Failed updating %s: %s<br>' % (dic['name'], `e`))
+      task.delete()
+      return
 
 class CheckUpdate(webapp.RequestHandler):
   def get(self):
@@ -71,7 +85,9 @@ class CheckUpdate(webapp.RequestHandler):
         assert result.headers.has_key('Content-Range')
         lastmod = memcache.get("lastmod-" + name)
         if lastmod is None or lastmod != result.headers['Last-Modified']:
-          MyQueue.push(name, type=MyQueue.BEGIN_FETCH, url=url)
+          totallen = result.headers['Content-Range']
+          totallen = int(totallen[totallen.index('/')+1:])
+          MyQueue.push(name, offset=0, totallen=totallen, url=url)
           if not memcache.set("lastmod-"+name, result.headers['Last-Modified']):
             logging.error("Memcache set failed for lastmod-%s." % (name))
           self.response.out.write('updated and added to queue.<br>')
@@ -87,6 +103,10 @@ class MainPage(webapp.RequestHandler):
     self.response.out.write('<html><body>\n')
     for dic in config['dictlist']:
       self.response.out.write('<p>name=%s<br>URL=%s</p>\n' % (dic['name'], dic['url']))
+    self.response.out.write('<hr/>')
+    results = TaskMessage.gql("ORDER BY timestamp") or []
+    for task in results:
+      self.response.out.write('<p>queue=%s, url=%s, offset=%d, totallen=%d, timestamp=%s</p>' % (task.queue_name, task.url, task.offset, task.totallen, task.timestamp))
     self.response.out.write('</body></html>')
 
 def main():
